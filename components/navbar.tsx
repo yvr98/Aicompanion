3a63eaa460602f3ed6a6b14a2c1712cb10ab57b3
yvr98 <yudthesvar98@gmail.com>
2023-08-08 17:05:49 -0400

    Added Dark mode and user button to nav bar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,18 @@
 "use client";
-import {Menu} from "lucide-react"
+import {Menu, Sparkles} from "lucide-react"
+import { Poppins } from "next/font/google";
 import Link from "next/link"
+import { UserButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+
+import { cn } from "@/lib/utils";
+import { ModeToggle } from "./mode-toggle";
+
+
+const font = Poppins({
+  weight:"600",
+  subsets: ["latin"]
+})
 
 export const Navbar = () => {
     return(
@@ -8,8 +20,16 @@ export const Navbar = () => {
         <div className="flex items-center">
           <Menu className="block md:hidden"/>
           <Link href="/">
-            <h1>Ai companion</h1>
+            <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary",font.className)}>Ai companion</h1>
           </Link>
+        </div>
+        <div className="flex items-center gap-x-3">
+          <Button variant="premium" size="sm">
+            Upgrade
+            <Sparkles className="h-4 w-4 fill-white text-white ml-2"/>
+          </Button>
+          <ModeToggle/>
+          <UserButton/>
         </div>
        </div>
     );
